77a8097d143fee43711d64e095725d2e077cdc6e
ujwalsai3337 <ujwal3337@gmail.com>
2024-08-23 15:01:21 +0530

    jai

---
+++ server/leetcodeprac.py
@@ -0,0 +1,27 @@
+
+
+
+# def reverseVowels( s: str) -> str:
+#     a=[]
+#     b=[]
+#     v = 'aeiouAEIOU'
+#     for i in range(len(s)):
+#         if s[i] in v:
+#             a.append(s[i])
+#     a = a[::-1]
+#     b = list(s)
+#     for i in range(len(b)):
+#         if b[i] in v:
+#             b[i]=a[0]
+#             a.pop(0)
+#     k=''.join(b)
+#     print(k)
+
+# reverseVowels("hello")
+
+
+
+
+s="sdfsf"
+b=list(s)
+print(b)
